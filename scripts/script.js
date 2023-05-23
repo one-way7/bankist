2a122cd88e63ae4bbce9f0c336604e61a791bf4f
Vlad <vladislav.stan.1998@gmail.com>
2023-05-23 19:11:55 +0300

    add smooth scroll to the section__header elem

--- scripts/script.js
+++ scripts/script.js
@@ -1,28 +1,37 @@
 'use strict';
 
-const modal = document.querySelector('.modal');
-const overlay = document.querySelector('.overlay');
-const btnCloseModal = document.querySelector('.btn--close-modal');
-const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+document.addEventListener('DOMContentLoaded', () => {
+    const modal = document.querySelector('.modal');
+    const overlay = document.querySelector('.overlay');
+    const btnCloseModal = document.querySelector('.btn--close-modal');
+    const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+    const btnScrollTo = document.querySelector('.btn--scroll-to');
+    const section1 = document.querySelector('#section--1');
 
-const openModal = function () {
-    modal.classList.remove('hidden');
-    overlay.classList.remove('hidden');
-};
+    const openModal = function (e) {
+        e.preventDefault();
 
-const closeModal = function () {
-    modal.classList.add('hidden');
-    overlay.classList.add('hidden');
-};
+        modal.classList.remove('hidden');
+        overlay.classList.remove('hidden');
+    };
 
-for (let i = 0; i < btnsOpenModal.length; i++)
-    btnsOpenModal[i].addEventListener('click', openModal);
+    const closeModal = function () {
+        modal.classList.add('hidden');
+        overlay.classList.add('hidden');
+    };
 
-btnCloseModal.addEventListener('click', closeModal);
-overlay.addEventListener('click', closeModal);
+    btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal));
 
-document.addEventListener('keydown', function (e) {
-    if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
-        closeModal();
-    }
+    btnCloseModal.addEventListener('click', closeModal);
+    overlay.addEventListener('click', closeModal);
+
+    document.addEventListener('keydown', function (e) {
+        if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+            closeModal();
+        }
+    });
+
+    btnScrollTo.addEventListener('click', () => {
+        section1.scrollIntoView({ behavior: 'smooth' });
+    });
 });
