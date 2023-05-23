e19317d8c99bc15fdc5a6345a2281c51524f4283
Vlad <vladislav.stan.1998@gmail.com>
2023-05-23 22:03:56 +0300

    implemented tabs in section 2

--- scripts/script.js
+++ scripts/script.js
@@ -8,6 +8,11 @@ document.addEventListener('DOMContentLoaded', () => {
     const btnScrollTo = document.querySelector('.btn--scroll-to');
     const section1 = document.querySelector('#section--1');
     const navLinks = document.querySelector('.nav__links');
+    const tabsContainer = document.querySelector('.operations__tab-container');
+    const tabsOperations = document.querySelectorAll('.operations__tab');
+    const contentsOperations = document.querySelectorAll(
+        '.operations__content',
+    );
 
     const openModal = function (e) {
         e.preventDefault();
@@ -42,5 +47,24 @@ document.addEventListener('DOMContentLoaded', () => {
             const id = e.target.getAttribute('href');
             document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
         }
+    });
+
+    tabsContainer.addEventListener('click', e => {
+        const tab = e.target.closest('.operations__tab');
+
+        if (!tab) return;
+
+        tabsOperations.forEach(tab =>
+            tab.classList.remove('operations__tab--active'),
+        );
+        tab.classList.add('operations__tab--active');
+
+        contentsOperations.forEach(content =>
+            content.classList.remove('operations__content--active'),
+        );
+        console.log(tab.dataset.tab);
+        document
+            .querySelector(`.operations__content--${tab.dataset.tab}`)
+            .classList.add('operations__content--active');
     });
 });
